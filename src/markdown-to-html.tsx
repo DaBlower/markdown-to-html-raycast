c50cdbb29300a897c06212ca8631d1a150a7de27
DaBlower <113282214+DaBlower@users.noreply.github.com>
2025-08-16 09:02:11 +1000

    implement md to html conversion

--- src/markdown-to-html.tsx
+++ src/markdown-to-html.tsx
@@ -1,8 +1,10 @@
 import { Detail, Clipboard } from "@raycast/api";
 import {useEffect, useState } from "react";
+import { marked } from 'marked';
 
 export default function Command() {
   const [text, setText] = useState("Getting contents of your clipboard...");
+  const [html, setHtml] = useState("")
 
   useEffect(() => {
     let cancelled = false; // cleanup if the command closes before finishing
@@ -11,7 +13,14 @@ export default function Command() {
         // Add delay to see the loading message
         await new Promise(resolve => setTimeout(resolve, 1000));
         const t = await Clipboard.readText();
-        if (!cancelled) setText(t ?? "Clipboard is empty or not text");
+        if (!cancelled) {
+          setText(t ?? "Clipboard is empty or not text");
+          if (t) { // if text exists, then convert it with marked
+            const convertedHtml = await marked(t)
+            console.log(await marked("# My heading"));
+            setHtml(convertedHtml)
+          }
+        }
       }
       catch {
         if (!cancelled) setText("Failed to read clipboard");
@@ -23,5 +32,5 @@ export default function Command() {
     };
   }, []);
   
-  return <Detail markdown={`# Clipboard\n\n${text}`} />;
+  return <Detail markdown={`# Clipboard\n\n${text}\n\n# HTML Output\n\n\`\`\`\n${html}\`\`\``} />;
 }
