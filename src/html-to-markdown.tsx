9a248f796e59e3a6df19c47f58e82cc4ba281a50
DaBlower <113282214+DaBlower@users.noreply.github.com>
2025-08-16 20:42:33 +1000

    improve clipboard loading feedback

--- src/html-to-markdown.tsx
+++ src/html-to-markdown.tsx
@@ -1,36 +1,52 @@
-import { Detail, Clipboard } from "@raycast/api";
+import { Clipboard, showHUD, closeMainWindow, Detail } from "@raycast/api";
 import { useEffect, useState } from "react";
 import TurndownService from 'turndown';
 
 export default function Command() {
-  const [text, setText] = useState("Getting contents of your clipboard...");
-  const [markdown, setMarkdown] = useState("")
-
+  const [status, setStatus] = useState("Loading clipboard")
   useEffect(() => {
+    // oscillating dots
+    const dots = [".", "..", "..."];
+    let dotIndex = 0;
+
+    const dotsInterval = setInterval(() =>{
+      dotIndex = (dotIndex + 1) % dots.length // modulo wraps it to the length of the dot array
+      setStatus(`Loading clipboard${dots[dotIndex]}`)
+    }, 300)
+
     let cancelled = false; // cleanup if the command closes before finishing
     (async () => {
+      await new Promise(resolve => setTimeout(resolve, 1250));
+      clearInterval(dotsInterval);
+      await closeMainWindow({ clearRootSearch: true });
       try {
         const t = await Clipboard.readText();
         if (!cancelled) {
           const trimmedText = t?.trim() ?? "Clipboard is empty or not text";
-          setText(trimmedText)
+
           if (trimmedText && trimmedText !== "Clipboard is empty or not text") { // if text exists, then convert it with marked
             const turndownService = new TurndownService();
             const convertedMarkdown = turndownService.turndown(trimmedText)
-            setMarkdown(convertedMarkdown)
+
             Clipboard.copy(convertedMarkdown)
+            await showHUD("Copied converted Markdown")
+          }
+          else {
+            await showHUD("Clipboard is empty or not text")
           }
         }
       }
+
       catch {
-        if (!cancelled) setText("Failed to read clipboard");
+        if (!cancelled) await showHUD("Failed to read clipboard");
       }
     })();
 
     return () => {
+      clearInterval(dotsInterval);
       cancelled = true;
     };
   }, []);
   
-  return <Detail markdown={`# Clipboard\n\n${text}\n\n# Markdown Output\n\n\`\`\`\n${markdown}\`\`\``} />;
+  return <Detail markdown={`# ${status}`}/>;
 }
